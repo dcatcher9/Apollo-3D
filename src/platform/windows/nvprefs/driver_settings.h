aea99f5d8e356bef10890f2a48a6b1c9371d6a2b
Yang Qu <dcatcher.qu@gmail.com>
2026-08-17 15:37:39 -0700

    chore(deps): update NVAPI SDK to R560

--- src/platform/windows/nvprefs/driver_settings.h
+++ src/platform/windows/nvprefs/driver_settings.h
@@ -8,8 +8,19 @@
 // disable clang-format header reordering
 // as <NvApiDriverSettings.h> needs types from <nvapi.h>
 // clang-format off
+#if defined(__MINGW32__) && !defined(__NVAPI_EMPTY_SAL)
+// R560's split lite headers nest their SAL start/end shims. Defer their
+// cleanup so an inner header cannot remove annotations still used by nvapi.h.
+#define SUNSHINE_NVAPI_END_EMPTY_SAL
+#define __NVAPI_EMPTY_SAL
+#endif
 #include <nvapi.h>
 #include <NvApiDriverSettings.h>
+#ifdef SUNSHINE_NVAPI_END_EMPTY_SAL
+#undef __NVAPI_EMPTY_SAL
+#include <nvapi_lite_salend.h>
+#undef SUNSHINE_NVAPI_END_EMPTY_SAL
+#endif
 // clang-format on
 
 // local includes
